c2c1a5a418661e3240a3a6d66bb0600d65a3d871
Ricardo Wu <44966248+WisleyDoma@users.noreply.github.com>
2022-02-19 08:41:18 -0800

    100% Swift-client established (#1)
    * Update MyLibrary.swift
    
    * Update MyLibrary.swift
    
    * 50% of statement printed
    
    testHello could print, but not testWeather
    
    * 100% print statement achieved
    
    switch number from 8 -> 0, was able to see testWeather running
    
    * Removed MockWeatherService.swift
    
    Co-authored-by: Ricardo Wu <wuhun@oregonstate.edu>

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -2,14 +2,15 @@ public class MyLibrary {
     private let weatherService: WeatherService
 
     /// The class's initializer.
-    /// 
+    ///  PR testing
+    ///  Ricardo's pull request
     /// Whenever we call the `MyLibrary()` constructor to instantiate a `MyLibrary` instance,
     /// the runtime then calls this initializer.  The constructor returns after the initializer returns.
     public init(weatherService: WeatherService? = nil) {
         self.weatherService = weatherService ?? WeatherServiceImpl()
     }
 
-    public func isLucky(_ number: Int, completion: @escaping (Bool?) -> Void) {
+    public func theWeather(_ number: Int, completion: @escaping (Bool?) -> Void) {
         // Check the simple case first: 3, 5 and 8 are automatically lucky.
         if number == 3 || number == 5 || number == 8 {
             completion(true)
@@ -18,19 +19,38 @@ public class MyLibrary {
 
         // Fetch the current weather from the backend.
         // If the current temperature, in Farenheit, contains an 8, then that's lucky.
-        weatherService.getTemperature { response in
+        weatherService.getWeather { response in
             switch response {
             case let .failure(error):
                 print(error)
                 completion(nil)
 
             case let .success(temperature):
+                print(temperature)
+                // print("test phase 001")
                 if self.contains(temperature, "8") {
                     completion(true)
                 } else {
                     let isLuckyNumber = self.contains(temperature, "8")
                     completion(isLuckyNumber)
                 }
+            }
+        }
+    }
+    
+    public func theGreetings (completion: @escaping (String?) -> Void) {
+
+        // Fetch the current weather from the backend.
+        // If the current temperature, in Farenheit, contains an 8, then that's lucky.
+        weatherService.getHello { response in
+            switch response {
+            case let .failure(error):
+                print(error)
+                completion(nil)
+
+            case let .success(greetings):
+                print(greetings)
+                completion(greetings)
             }
         }
     }
--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -1,31 +1,127 @@
 import Alamofire
 
+
+
 public protocol WeatherService {
-    func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void)
+    func getWeather(completionHandler: @escaping (_ response: Result <Int /*Hello string line*/, Error>) -> Void)
+    func getHello(completionHandler: @escaping (_ response: Result <String /*Hello string line*/, Error>) -> Void)
+    func fetchToken(completionHandler: @escaping (_ response: Result <String /* Token & expr date */, Error>) -> Void)
 }
 
 class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
-
-    func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
-        AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
+    let weather_url = "http://44.197.118.88:3000/v1/weather"
+    let hello_url = "http://44.197.118.88:3000/v1/hello"
+    let token_url = "http://44.197.118.88:3000/v1/auth"
+    // function of v1/weather
+    func getWeather(completionHandler: @escaping (_ response: Result <Int /*Hello string line*/, Error>) -> Void){
+        fetchToken(completionHandler: { response in
+            switch response {
+            case .failure(let error):
+                completionHandler(.failure(error))
+                // print("error: \(error)")
+                // break
+            case .success(let my_token):
+                let token_headers : HTTPHeaders = [.authorization(bearerToken: my_token), .accept("application/json")]
+                // completionHandler(.success(my_token))
+                // print(my_token)
+                // break
+                //
+                    AF.request(self.weather_url, method: .get, headers: token_headers).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
+                                    switch response.result{
+                                    case .success(let weather):
+                                        let temperature = weather.main.temp
+                                        let temperatureAsInteger = Int(temperature)
+                                        completionHandler(.success(temperatureAsInteger))
+                                    case .failure(let error):
+                                        completionHandler(.failure(error))
+                                    }
+                        }
+                }
+        }
+        )
+    }
+    // function of v1/hello
+    func getHello(completionHandler: @escaping (_ response: Result <String /*Hello string line*/, Error>) -> Void){
+        //
+        fetchToken(completionHandler: { response in
+            switch response {
+            case .failure(let error):
+                completionHandler(.failure(error))
+                // print("error: \(error)")
+                // break
+            case .success(let my_token):
+                let token_headers : HTTPHeaders = [.authorization(bearerToken: my_token), .accept("application/json")]
+                // completionHandler(.success(my_token))
+                // print(my_token)
+                // break
+                //
+                AF.request(self.hello_url, method: .get, headers: token_headers).validate(statusCode: 200..<300).responseDecodable(of: Hello.self) { response in
+                            switch response.result{
+                            case .success(let her_hello):
+                                completionHandler(.success(her_hello.message))
+                                // print(her_hello)
+                                // break
+                            case .failure(let ghost):
+                                completionHandler(.failure(ghost))
+                                // print("error: \(ghost)")
+                                // break
+                            }
+                        }
+                }
+        }
+        )
+    }
+    let payload = Payload(
+      username: "Ricardo",
+      password: "o2LAJH12Y2!kq1dA")
+    // function of v1/auth
+    func fetchToken(completionHandler: @escaping (_ response: Result <String /* Token & expr date */, Error>) -> Void){
+        //
+        AF.request(token_url, method: .post, parameters: payload, encoder: JSONParameterEncoder.default).validate(statusCode: 200..<300).responseDecodable(of: Authentication.self){ response in
             switch response.result {
-            case let .success(weather):
-                let temperature = weather.main.temp
-                let temperatureAsInteger = Int(temperature)
-                completion(.success(temperatureAsInteger))
-
-            case let .failure(error):
-                completion(.failure(error))
+            case .success(let my_token):
+                completionHandler(.success(my_token.token))
+                // print(my_token)
+                // break
+            case .failure(let error):
+                completionHandler(.failure(error))
+                // print("error: \(error)")
+                // break
             }
         }
     }
 }
 
+// constructors:
 private struct Weather: Decodable {
     let main: Main
-
+    
     struct Main: Decodable {
         let temp: Double
     }
+}
+
+struct Hello: Codable {
+    let message: String
+}
+
+// Codable: make the JSON decodable & codable here.
+struct Authentication: Decodable{
+    var token, expiration_date: String
+    
+    enum CodingKeys: String, CodingKey {
+            case token = "access-token"
+            case expiration_date = "expires"
+        }
+}
+
+// constructor for user inputs
+struct Payload: Encodable {
+  let username: String
+  let password: String
+}
+
+// The message shouldn't be mutable, instead I'll use let type.
+struct Display_message: Decodable{
+    let message: String
 }
